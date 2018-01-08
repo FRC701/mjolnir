3fa632efcfaaf3c8be15f90cccae50839893c3b7
NWSchallock <nicschallock@gmail.com>
2018-01-07 16:27:15 -0800

    Changes to set up InitialCode

---
+++ src/Commands/SetIntake.cpp
@@ -0,0 +1,34 @@
+#include "SetIntake.h"
+#include "Subsystems/Intake.h"
+
+SetIntake::SetIntake(double speed): mSpeed(speed) {
+	// Use Requires() here to declare subsystem dependencies
+	// eg. Requires(Robot::chassis.get());
+	Requires(Intake::getInstance().get());
+}
+
+// Called just before this Command runs the first time
+void SetIntake::Initialize() {
+
+}
+
+// Called repeatedly when this Command is scheduled to run
+void SetIntake::Execute() {
+	Intake::getInstance()->SetIntake(mSpeed);
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool SetIntake::IsFinished() {
+	return false;
+}
+
+// Called once after isFinished returns true
+void SetIntake::End() {
+
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void SetIntake::Interrupted() {
+
+}
