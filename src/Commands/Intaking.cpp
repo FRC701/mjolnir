64c5a34c4a3a51a389b70b9a17819f0be7c15956
MajorDarius <majorminor777@hotmail.com>
2018-02-17 14:20:36 -0800

    Intake Group Command WIP

---
+++ src/Commands/Intaking.cpp
@@ -0,0 +1,34 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "Intaking.h"
+#include "SetArmPosition.h"
+#include "IntakeEngage.h"
+#include "SetIntake.h"
+
+Intaking::Intaking() {
+
+  AddSequential(new SetArmPosition(0.0));
+  AddSequential(new IntakeEngage());
+  AddSequential(new SetIntake(1.0));
+	// Add Commands here:
+	// e.g. AddSequential(new Command1());
+	//      AddSequential(new Command2());
+	// these will run in order.
+
+	// To run multiple commands at the same time,
+	// use AddParallel()
+	// e.g. AddParallel(new Command1());
+	//      AddSequential(new Command2());
+	// Command1 and Command2 will run in parallel.
+
+	// A command group will require all of the subsystems that each member
+	// would require.
+	// e.g. if Command1 requires chassis, and Command2 requires arm,
+	// a CommandGroup containing them would require both the chassis and the
+	// arm.
+}
