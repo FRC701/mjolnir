527fff1ea21692aed29570a63f854af902feadf8
Mr. Murphy <dmurphy@vandenrobotics.com>
2018-03-03 17:19:15 -0800

    Left Scale Auto

---
+++ src/Commands/AutoLeftScale.h
@@ -0,0 +1,11 @@
+#ifndef AutoLeftScale_H
+#define AutoLeftScale_H
+
+#include "ChassisMotionProfileCommand.h"
+
+class AutoLeftScale : public ChassisMotionProfileCommand {
+public:
+  AutoLeftScale();
+};
+
+#endif  // AutoLeftScale_H
