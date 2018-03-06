e6bce395a1813b3e661909673c8dc98c7f1eddef
MajorDarius <majorminor777@hotmail.com>
2018-03-05 16:31:18 -0800

    Added an AutoSwitchSelector command

---
+++ src/Commands/AutoSwitchSelector.cpp
@@ -0,0 +1,35 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "AutoSwitchSelector.h"
+#include "GameData.h"
+
+#include "Commands/Scheduler.h"
+
+using robovikes::GameData;
+
+AutoSwitchSelector::AutoSwitchSelector(
+    frc::Command* _leftCommand,
+    frc::Command* _rightCommand)
+: leftCommand(_leftCommand),
+  rightCommand(_rightCommand)
+{
+	// Use Requires() here to declare subsystem dependencies
+	// eg. Requires(Robot::chassis.get());
+}
+
+// Called just before this Command runs the first time
+void AutoSwitchSelector::Initialize() {
+  GameData::Side side = GameData::GetOurSwitchSide();
+
+  if (side == GameData::Left) {
+    frc::Scheduler::GetInstance()->AddCommand(leftCommand);
+  } else if (side == GameData::Right) {
+    frc::Scheduler::GetInstance()->AddCommand(rightCommand);
+  }
+}
+
---
+++ src/Commands/AutoSwitchSelector.h
@@ -0,0 +1,22 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <Commands/InstantCommand.h>
+
+class AutoSwitchSelector : public frc::InstantCommand {
+private:
+    frc::Command* leftCommand;
+    frc::Command* rightCommand;
+public:
+	AutoSwitchSelector(
+	    frc::Command* leftCommand,
+	    frc::Command* rightCommand);
+	void Initialize() override;
+};
+
