77e6218324737e47e36a315eb42c75d5e52c20b6
NWSchallock <nics@vandenrobotics.com>
2018-03-09 12:14:27 -0800

    Full Left Scale

--- src/Commands/AutoFullLeftScale.cpp
+++ src/Commands/AutoFullLeftScale.cpp
@@ -5,26 +5,23 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-#include "Intaking.h"
-#include "SetArmPosition.h"
-#include "IntakeEngage.h"
-#include "SetIntake.h"
-#include "DrawSling.h"
+#include "AutoFullLeftScale.h"
+#include "AutoLeftScale.h"
 #include "Delay.h"
-#include "SetArmPosConditional.h"
-#include "SetPuncherPos.h"
+#include "DrawSling.h"
+#include "IntakeDisengage.h"
+#include "SetArmPosition.h"
+#include "SetSlingDisengagement.h"
+#include "SetIntake.h"
+#include "SetSlingDisengagement.h"
 
-Intaking::Intaking() {
-
-  //AddParallel(new SetPuncherPos(7000));
-  AddParallel(new IntakeEngage());
-  AddSequential(new SetArmPosConditional(7500));
-  AddSequential(new Delay(0.25));
-  AddParallel(new SetArmPosition(0));
-  AddSequential(new SetIntake(1.0));
+AutoFullLeftScale::AutoFullLeftScale() {
+  AddSequential(new AutoLeftScale());
+  AddSequential(new SetArmPosition(11750));
+  AddSequential(new DrawSling(21156));
+  AddSequential(new IntakeDisengage());
   AddSequential(new Delay(0.5));
-  AddParallel(new SetArmPosition(9500)); //7700
-  AddSequential(new SetIntake(0.0));
+  AddSequential(new SetSlingDisengagement());
 	// Add Commands here:
 	// e.g. AddSequential(new Command1());
 	//      AddSequential(new Command2());
@@ -35,6 +32,7 @@ Intaking::Intaking() {
 	// e.g. AddParallel(new Command1());
 	//      AddSequential(new Command2());
 	// Command1 and Command2 will run in parallel.
+
 	// A command group will require all of the subsystems that each member
 	// would require.
 	// e.g. if Command1 requires chassis, and Command2 requires arm,
