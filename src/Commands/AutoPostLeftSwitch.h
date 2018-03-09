a2f6d8a6a9c325f3e04faa71af4acef6dc772a5a
NWSchallock <nics@vandenrobotics.com>
2018-03-09 11:49:03 -0800

    Add AutoPostLeftSwitch

---
+++ src/Commands/AutoPostLeftSwitch.h
@@ -0,0 +1,11 @@
+#ifndef AutoPostLeftSwitch_H
+#define AutoPostLeftSwitch_H
+
+#include "ChassisMotionProfileCommand.h"
+
+class AutoPostLeftSwitch : public ChassisMotionProfileCommand {
+public:
+  AutoPostLeftSwitch();
+};
+
+#endif  // AutoPostLeftSwitch_H
